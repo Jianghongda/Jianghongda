07cfcbafdf2766a9ded0b73fbd7ce717d0820457
hongda <18331394971@163.com>
2024-10-14 17:08:42 +0800

    循环，数组，哈希map

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Test extends Person{
     public static void main(String args[]){
         Test t = new Test();
@@ -7,5 +9,57 @@ public class Test extends Person{
         t.setAge(25);
         //t.setSalary(10000.0); //不能调用
         t.getInfo();
+        int[] myArray = new int[5];
+        for (int i =0; i <5; i++){
+            myArray[i] = 10*(i+1);
+        }
+        for (int i =0; i <5; i++){
+            System.out.println(myArray[i]);
+        }
+
+        String[][] students = {
+                {"张三", "18", "男"},
+                {"李四", "20", "女"},
+                {"王五", "19", "男"},
+                {"赵六", "17", "女"},
+                {"赵七", "22", "男"}
+        };
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("姓名：" + students[i][0] + "，年龄：" + students[i][1] + "，性别：" + students[i][2]);
+        }
+// 使用break终止循环
+        for (int i=0; i<10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+// 使用continue跳过循环
+        for (int i=0; i<10; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        for(int i =1; i<=10; i++){
+            for (int j=10; j>0;j--){
+                if(j>i){
+                    System.out.print(" ");
+                }else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+
+        }
+        HashMap<String, String> map = new HashMap<String, String>();
+        //put(key,value):添加元素
+        map.put("1", "One");
+        map.put("2", "Two");
+        map.put("3", "Three");
+
+
     }
 }

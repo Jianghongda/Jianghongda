00f1657f03648c730f53a4f4470f38cc875da053
hongda <18331394971@163.com>
2024-10-14 13:08:07 +0800

    添加demo类

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        demo student = new demo();
+        student.name = "zhangsan";
+        student.study();
+        student.play();
+    }
+
+}
---
+++ src/main/java/org/example/demo.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class demo {
+    String name;
+    int age;
+    String gender;
+
+    void study(){
+        System.out.println("Student"+name+"is studying.");
+    }
+
+    void play(){
+        System.out.println("Study"+name+"is playing.");
+    }
+
+}
